e6e7b9f9ecda2d59d148f9e91874723e006acb2b
Vyacheslav Voronin <goodwell2006@gmail.com>
2026-08-11 16:50:48 +0300

    sirst version

---
+++ scripts/data_type.f90
@@ -0,0 +1,50 @@
+module data_type
+
+    implicit none
+    
+    !Вектор трехмерного измерения
+    type :: vector3
+        real :: x
+        real :: y
+        real :: z
+    end type vector3
+
+    !Данные среды
+    type :: enviroment
+        integer :: count_nuclear = 0                                        ! Количество ядер
+        type(nuclear_data),allocatable :: different_tipe_of_nuclear(:)      ! Массив ядер
+    end type enviroment
+
+    !Данные отдельного ядра в среде 
+    type nuclear_data 
+        character(len=40) name_of_nuclie                                    ! Имя нуклида
+        integer index_of_nuclie                                             ! Индекс ядра
+        real mass_of_nuclear                                                ! Масса ядра 
+        real nuclear_dencity                                                ! Ядерная плотность
+        integer count_process                                               ! Количество процессов
+        integer count_point                                                 ! Количество точек в таблице
+        integer :: count_point_vel_distr = 0
+        real, allocatable :: energy_point_in_table(:)                       ! Столбец энергии (Общий для всех)
+        type(cross_section_data),allocatable:: cross_data(:)                ! Столбы различных сечений
+        real, dimension(:), allocatable ::coordinate_distribution_grid      ! Сетка по плотности вероятности ядер
+        real, dimension(:), allocatable ::coordinate_velocity_grid          ! Сетка по скоростям ядер
+    end type nuclear_data
+
+    !Таблица сечений для ядра 
+    type cross_section_data
+        integer :: index_of_process = -1                                    ! Индекс процесса
+        real, allocatable :: cross_section_point_in_table(:)                ! Стобец процесса
+    end type cross_section_data
+
+    !информация о нейтроне
+    type :: netron_data
+        type(vector3) :: dir                                                ! Направление движения
+        type(vector3) :: pos = vector3(0,0,0)                               ! Расположение в пространстве
+        real :: energy                                                      ! Энергия нейтрона, eV
+        real :: speed                                                       ! Скорость нейтрона
+        real :: life_time = 0                                               ! Время рассеяния
+        integer :: count_collision = 0                                      ! Количество испытанных столкновений
+        logical :: is_died = .False.                                        ! Флаг поглощенности нейтрона 
+    end type netron_data
+
+end module data_type
